d3cfae3f282f9ba71aa8112f05275c7d62964785
Gaborojers <gaborojers@gmail.com>
2024-01-29 23:38:09 -0600

    commit 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Inicio from './components/Login';
+import Inicio from './pages/Login';
 import Registro from './components/Registro';
 import Chat from './pages/Chat';
 
